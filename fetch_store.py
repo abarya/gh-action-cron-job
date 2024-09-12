daaf25a7b4f649dd0c08b1a50e08ea83ac180cdd
Abhishek Arya <abhishekarya286@gmail.com>
2024-09-12 15:36:48 +0100

    Update fetch_store.py

--- fetch_store.py
+++ fetch_store.py
@@ -1,19 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 import requests
 
-# Open the website to get cookies
-url = "https://www.myvue.com"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
-resp = requests.get(url, headers=headers)
-print("web response", resp.reason)
-print(resp.cookies)
+# Initialize the WebDriver (using Chrome in this example)
+driver = webdriver.Chrome()#executable_path='/home/abhishek/Downloads/chromedriver-linux64/chromedriver')
 
+# Open the website
 apiUrl = "https://www.myvue.com/api/microservice/showings/cinemas/10004" + "/films?filmId=" + "HO00019557" + "&minEmbargoLevel=1&includesSession=true&includeSessionAttributes=true"
-resp = requests.get(apiUrl, cookies=resp.cookies)
+url = "https://www.myvue.com"
+driver.get(url)
 
+# Optionally, wait for the page to load completely or perform actions like login
+time.sleep(5)  # Adjust this based on loading time or add WebDriverWait for specific elements
+
+# Retrieve all cookies from the browser
+cookies = driver.get_cookies()
+cookies_dict = {cookie['name']: cookie['value'] for cookie in cookies}
+
+resp = requests.get(apiUrl, cookies=cookies_dict)
 if resp.status_code != 200:
     print(resp)
-
 print(resp.json())
 
+# Close the browser
+driver.quit()
