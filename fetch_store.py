0cfcd093d67d2adf057df96c55c13cc7749f35a8
Abhishek Arya <abhishekarya286@gmail.com>
2024-09-12 15:07:31 +0100

    Update fetch_store.py

--- fetch_store.py
+++ fetch_store.py
@@ -3,6 +3,8 @@ import requests
 # Open the website to get cookies
 url = "https://www.myvue.com"
 resp = requests.get(url)
+print("web response", resp)
+print(resp.cookies)
 
 apiUrl = "https://www.myvue.com/api/microservice/showings/cinemas/10004" + "/films?filmId=" + "HO00019557" + "&minEmbargoLevel=1&includesSession=true&includeSessionAttributes=true"
 resp = requests.get(apiUrl, cookies=resp.cookies)
