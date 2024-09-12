67805c6f49768d91af9e475b1b4ba4c27ecdb94b
Abhishek Arya <abhishekarya286@gmail.com>
2024-09-12 15:40:58 +0100

    Update fetch_store.py

--- fetch_store.py
+++ fetch_store.py
@@ -2,9 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import requests
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")  # Applicable for older versions of Chrome
+chrome_options.add_argument("--window-size=1920,1080")
 
 # Initialize the WebDriver (using Chrome in this example)
-driver = webdriver.Chrome()#executable_path='/home/abhishek/Downloads/chromedriver-linux64/chromedriver')
+driver = webdriver.Chrome(options=chrome_options)#executable_path='/home/abhishek/Downloads/chromedriver-linux64/chromedriver')
 
 # Open the website
 apiUrl = "https://www.myvue.com/api/microservice/showings/cinemas/10004" + "/films?filmId=" + "HO00019557" + "&minEmbargoLevel=1&includesSession=true&includeSessionAttributes=true"
