1214af86a6c9fe296e240d9cdd4976a6719d6431
Abhishek Arya <abhishekarya286@gmail.com>
2024-09-12 15:05:27 +0100

    Create fetch_store.py

---
+++ fetch_store.py
@@ -0,0 +1,14 @@
+import requests
+
+# Open the website to get cookies
+url = "https://www.myvue.com"
+resp = requests.get(url)
+
+apiUrl = "https://www.myvue.com/api/microservice/showings/cinemas/10004" + "/films?filmId=" + "HO00019557" + "&minEmbargoLevel=1&includesSession=true&includeSessionAttributes=true"
+resp = requests.get(apiUrl, cookies=resp.cookies)
+
+if resp.status_code != 200:
+    print(resp)
+
+print(resp.json())
+
